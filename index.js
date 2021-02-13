cf5cab58a8bc93e864bb1ac512b2e407170e2972
Heath123 <heath.mitchell27@gmail.com>
2021-02-13 18:41:52 +0100

    Actaully works now

--- index.js
+++ index.js
@@ -1,13 +1,64 @@
 require('dotenv').config()
-const { Octokit } = require('@octokit/rest');
+const { Octokit } = require('@octokit/rest')
+const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN })
+const fs = require('fs')
 
-(async () => {
-  const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN })
+// https://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep
+function sleep (ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
 
-  console.log(await octokit.search.commits({
-    q: 'query'
-  }))
-})()
+// TODO: Reduce memory usage if needed?
+if (!fs.existsSync('./alreadyCommented.json')) {
+  fs.writeFileSync('./alreadyCommented.json', '[]')
+}
 
-// Makes debugger work
-setTimeout(() => {}, 1000000)
+function saveJSON () {
+  fs.writeFileSync('./alreadyCommented.json', JSON.stringify(alreadyCommented))
+}
+
+const alreadyCommented = JSON.parse(fs.readFileSync('./alreadyCommented.json'))
+
+const message = fs.readFileSync('message.txt', { encoding: 'utf8' })
+
+async function commentFromItem (item) {
+  await commentOnIfNotDone(item.repository.owner.login, item.repository.name, item.sha, item.commit.author.name)
+}
+
+async function commentOnIfNotDone (repoOwner, repo, commitId, commitAuthor) {
+  if (alreadyCommented.includes(commitId)) return
+
+  await octokit.repos.createCommitComment({
+    owner: repoOwner,
+    repo: repo,
+    commit_sha: commitId,
+    body: message.replace('${username}', commitAuthor)
+  })
+
+  // Don't trigger rate limits
+  // TODO: Do this properly
+  await sleep(2000)
+
+  alreadyCommented.push(commitId)
+  saveJSON()
+  console.log('Commented on', commitId)
+}
+
+async function searchAndComment () {
+  console.log('Searching...')
+  const commits = await octokit.search.commits({
+    q: '"remove password" OR "remove token"',
+    sort: 'committer-date'
+  })
+
+  for (const commit of commits.data.items) {
+    try {
+      await commentFromItem(commit)
+    } catch (error) {
+      console.error(error, commit)
+    }
+  }
+}
+
+setInterval(searchAndComment, 60 * 1000)
+searchAndComment()
