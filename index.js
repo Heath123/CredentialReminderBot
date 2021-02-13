d8611752c40e17fca24ff22c1691c85735244d34
Heath123 <heath.mitchell27@gmail.com>
2021-02-13 17:31:11 +0100

    Uplaod commit search code, but I'm going to switch to OctoKit soon

--- index.js
+++ index.js
@@ -6,5 +6,24 @@ const gh = new GitHub({
   password: process.env.GITHUB_PASSWORD
 })
 
-const user = gh.getUser()
-console.log(user)
+// The github-api doesn't seem to support this in cleaner way than this?
+// TODO: auth seems to be broken, causing a strict rate limit
+function searchCommits (gh, options) {
+  return new Promise((resolve, reject) => {
+    const search = gh.search({})
+    search.__AcceptHeader = 'cloak-preview'
+    search._search('commits', options, (err, result) => {
+      if (err) {
+        console.error(result)
+        reject(err)
+      }
+      if (result) resolve(result)
+    })
+  })
+}
+
+(async () => {
+  const user = gh.getUser()
+  console.log(user)
+  console.log(await searchCommits(gh, { q: '"remove password" OR "remove token"' }))
+})()
